61f9853e4daf828b2349446746457b7b7f254a4b
AlinaKhirullina <alinakhairullina200407@gmail.com>
2024-02-11 19:45:05 +0300

    change qsort.c

--- C/features/qsort.c
+++ C/features/qsort.c
@@ -1,46 +1,31 @@
 #include <stdio.h>
 
 void qsort(int *mas, int size) {
-    //Указатели в начало и в конец массива
     int i = 0;
     int j = size - 1;
-
-    //Центральный элемент массива
     int mid = mas[size / 2];
-
-    //Делим массив
-    do {
-        //Пробегаем элементы, ищем те, которые нужно перекинуть в другую часть
-        //В левой части массива пропускаем(оставляем на месте) элементы, которые меньше центрального
-        while(mas[i] < mid) {
+    int tmp;
+    do 
+    {
+        while(mas[i] < mid)
             i++;
-        }
-        //В правой части пропускаем элементы, которые больше центрального
-        while(mas[j] > mid) {
+        while(mas[j] > mid) 
             j--;
-        }
-
-        //Меняем элементы местами
-        if (i <= j) {
-            int tmp = mas[i];
+        if (i <= j) 
+        {
+            tmp = mas[i];
             mas[i] = mas[j];
             mas[j] = tmp;
-
             i++;
             j--;
         }
     } while (i <= j);
-
-
-    //Рекурсивные вызовы, если осталось, что сортировать
-    if(j > 0) {
-        //"Левый кусок"
+    if(j > 0)
+        //left
         qsort(mas, j + 1);
-    }
-    if (i < size) {
-        //"Првый кусок"
+    if (i < size)
+        //right
         qsort(&mas[i], size - i);
-    }
 }
 int main(int argc, char ** argv)
 {
