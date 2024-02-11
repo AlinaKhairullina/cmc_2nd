6dfa6b67d983fe93ba6f316dd1a5296ece3d3443
AlinaKhirullina <alinakhairullina200407@gmail.com>
2024-02-11 19:55:34 +0300

    add comment thread.c

--- C/features/thread.c
+++ C/features/thread.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <time.h>
+/*Нужно создать нить и напечатать время ее работы*/
 void* thr_fn(void *arg)
 {
 sleep(time(0) % 10);
@@ -13,11 +15,12 @@ pthread_exit(NULL);
 int main(){
 pthread_t th_id, id;
 void *pret;
-pthread_create(&id, NULL, thr_fn, NULL);
-printf("%d\n", pthread_equal(th_id, id));
+pthread_create(&th_id, NULL, thr_fn, NULL);
+printf("The thread is created\n");
 time_t tm_start = time(0);
+printf("Wait thread\n");
 if (!pthread_join(th_id, &pret))
-    printf("Thread %#lx time =%lu\n", th_id, time(0) - tm_start);
+    printf("Thread %#lx time = %lu\n", th_id, time(0) - tm_start);
 else
     fprintf(stderr, "Error of pthread_join\n");
 }
